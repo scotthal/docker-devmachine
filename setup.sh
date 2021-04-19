9e615d4c97f84760e74db69b234b6a7e1f270297
Scott Hallock <scott@hallocks.net>
2021-04-19 23:35:32 +0000

    Install Roboto Mono and fix various bugs

--- setup.sh
+++ setup.sh
@@ -19,27 +19,53 @@ apt-get update; \
   locale-gen; \
   update-locale LANG=en_US.UTF-8
 
+su $USERNAME -lc "\
+  git clone https://github.com/scotthal/homedir; \
+  mv homedir/.git .; \
+  git reset --hard HEAD; \
+  mv .git homedir \
+"
+
+curl -L https://storage.googleapis.com/scotthal-devmachine-public/Roboto_Mono.zip > /tmp/Roboto_Mono.zip; \
+  su $USERNAME -lc "\
+    mkdir .fonts; \
+    mkdir roboto-unz; \
+    cd roboto-unz; \
+    unzip /tmp/Roboto_Mono.zip; \
+    cp static/RobotoMono-Regular.ttf ../.fonts; \
+    cp static/RobotoMono-Medium.ttf ../.fonts; \
+    cd ..; \
+    rm -rf roboto-unz
+  "
+
+rm -f /tmp/Roboto_Mono.zip
+
 apt-get -y install lightdm; \
   apt-get -y install xubuntu-desktop fonts-roboto firefox docker.io; \
 
 usermod -a -G docker $USERNAME
 
-mkdir -p /home/scotthal/bin; \
-  chown scotthal:scotthal /home/scotthal/bin; \
-  chmod 0755 /home/scotthal/bin
+mkdir -p /home/$USERNAME/bin; \
+  chown $USRNAME:$USERNAME /home/$USERNAME/bin; \
+  chmod 0755 /home/$USERNAME/bin
 
 curl -L https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /tmp/crd.deb; \
-    dpkg -i /tmp/crd.deb; \
-    apt-get -y install -f; \
-    rm -f /tmp/crd.deb; \
-    curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb; \
-    dpkg -i /tmp/chrome.deb; \
-    apt-get -y install -f; \
-    rm -f /tmp/chrome.deb; \
-    update-alternatives --set x-www-browser /usr/bin/google-chrome-stable; \
-    curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /tmp/minikube; \
-    install -o scotthal -g scotthal /tmp/minikube /home/scotthal/bin; \
-    rm -f /tmp/minikube
+  dpkg -i /tmp/crd.deb; \
+  apt-get -y install -f; \
+  rm -f /tmp/crd.deb; \
+  curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb; \
+  dpkg -i /tmp/chrome.deb; \
+  apt-get -y install -f; \
+  rm -f /tmp/chrome.deb; \
+  update-alternatives --set x-www-browser /usr/bin/google-chrome-stable; \
+  curl -L https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_amd64.zip > /tmp/terraform.zip; \
+  mkdir /tmp/terraform-unz; \
+  unzip /tmp/terraform.zip -d /tmp/terraform-unz; \
+  install -o $USERNAME -g $USERNAME /tmp/terraform-unz/terraform /home/$USERNAME/bin/terraform; \
+  rm -rf /tmp/terraform.zip /tmp/terraform-unz; \
+  curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /tmp/minikube; \
+  install -o $USERNAME -g $USERNAME /tmp/minikube /home/$USERNAME/bin/minikube; \
+  rm -f /tmp/minikube
 
 snap install aws-cli --classic; \
   snap install code --classic; \
@@ -47,3 +73,5 @@ snap install aws-cli --classic; \
 
 apt-get autoremove -y ;\
   apt-get clean -y
+
+touch /tmp/setup-done
