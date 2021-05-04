1ac3ac0d04dd2b5498b27a100af22d733bdc76ab
Scott Hallock <scott@hallocks.net>
2021-05-04 16:03:57 +0000

    Imperfect desktop stability fixes
    - Install xscreensaver to prevent light-locker from crashing
    - Remove blueman as it isn't needed and crashes a lot
    - Set a default session in /etc/chrome-remote-desktop-session
    
    These increase the probability of getting a desktop to come up,
    but xfsettingsd is still crashing a lot.

--- setup.sh
+++ setup.sh
@@ -41,7 +41,12 @@ curl -L https://storage.googleapis.com/scotthal-devmachine-public/Roboto_Mono.zi
 rm -f /tmp/Roboto_Mono.zip
 
 apt-get -y install lightdm; \
-  apt-get -y install xubuntu-desktop fonts-roboto firefox docker.io docker-compose; \
+  apt-get -y install xubuntu-desktop xscreensaver fonts-roboto firefox docker.io docker-compose; \
+  apt-get -y remove blueman; \
+  systemctl service stop lightdm.service; \
+  systemctl service disable lightdm-service; \
+  systemctl service stop gdm.service; \
+  systemctl service disable gdm.service
 
 usermod -a -G docker $USERNAME
 
@@ -53,6 +58,7 @@ curl -L https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.d
   dpkg -i /tmp/crd.deb; \
   apt-get -y install -f; \
   rm -f /tmp/crd.deb; \
+  echo 'exec /usr/bin/xfce4-session' > /etc/chrome-remote-desktop-session; \
   curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb; \
   dpkg -i /tmp/chrome.deb; \
   apt-get -y install -f; \
