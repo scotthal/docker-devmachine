3d4f3bd875ee2e6a2bc410d16b7507c6abd39201
scotthal <scott@hallocks.net>
2021-09-06 09:00:37 -0600

    Alphabetize VS Code extensions

--- setup.sh
+++ setup.sh
@@ -99,10 +99,10 @@ su $USERNAME -lc "\
     code --install-extension ms-vscode.cpptools; \
     code --install-extension ms-vscode.powershell; \
     code --install-extension ms-vscode-remote.vscode-remote-extensionpack; \
+    code --install-extension pivotal.vscode-boot-dev-pack; \
     code --install-extension rust-lang.rust; \
     code --install-extension visualstudioexptteam.vscodeintellicode; \
-    code --install-extension vscjava.vscode-java-pack; \
-    code --install-extension pivotal.vscode-boot-dev-pack
+    code --install-extension vscjava.vscode-java-pack
   "
 
 apt-get autoremove -y ;\
