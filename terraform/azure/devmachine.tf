4575f1f9aa936a43137416e6bca073c53df4acb9
Scott Hallock <scott@hallocks.net>
2022-03-31 17:55:15 +0000

    Add a public IP address

--- terraform/azure/devmachine.tf
+++ terraform/azure/devmachine.tf
@@ -30,6 +30,13 @@ resource "azurerm_subnet" "dev" {
   address_prefixes     = ["10.101.0.0/24"]
 }
 
+resource "azurerm_public_ip" "dev" {
+  name                = "dev-machine-public-ip"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = azurerm_resource_group.dev.location
+  allocation_method   = "Dynamic"
+}
+
 resource "azurerm_network_interface" "dev" {
   name                = "dev-machine-network-interface"
   resource_group_name = azurerm_resource_group.dev.name
@@ -38,5 +45,6 @@ resource "azurerm_network_interface" "dev" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.dev.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.dev.id
   }
 }
