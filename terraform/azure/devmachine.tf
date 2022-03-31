7ca773851aacef072fa2fc7c307a1fbc7b8859ce
Scott Hallock <scott@hallocks.net>
2022-03-31 16:48:45 +0000

    Add virtual network and subnet

--- terraform/azure/devmachine.tf
+++ terraform/azure/devmachine.tf
@@ -15,3 +15,17 @@ resource "azurerm_resource_group" "dev" {
   name     = "dev-machine-resources"
   location = "West US 2"
 }
+
+resource "azurerm_virtual_network" "dev" {
+  name                = "dev-machine-network"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = azurerm_resource_group.dev.location
+  address_space       = ["10.101.0.0/16"]
+}
+
+resource "azurerm_subnet" "dev" {
+  name                 = "dev-machine-subnet"
+  resource_group_name  = azurerm_resource_group.dev.name
+  virtual_network_name = azurerm_virtual_network.dev.name
+  address_prefixes     = ["10.101.0.0/24"]
+}
