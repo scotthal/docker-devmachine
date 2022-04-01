e8abae35e462eb8dddb0ac232e074d5509501c46
Scott Hallock <scott@hallocks.net>
2022-04-01 22:58:49 +0000

    Add a security group allowing development traffic

--- terraform/aws/devmachine.tf
+++ terraform/aws/devmachine.tf
@@ -41,3 +41,42 @@ resource "aws_route_table_association" "dev_subnet_association" {
   subnet_id      = aws_subnet.dev.id
 }
 
+resource "aws_security_group" "dev" {
+  name        = "allow_dev_traffic"
+  description = "Allow traffic on common development ports"
+  vpc_id      = aws_vpc.dev.id
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "dev_http"
+    from_port   = 8000
+    to_port     = 8999
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
