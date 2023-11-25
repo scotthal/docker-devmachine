b2dec8ace86986a49049483a8a9310dbbf0f2975
Scott Hallock <scott@hallocks.net>
2023-11-25 19:33:29 +0000

    Install parallel

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME; \
 
 apt-get update; \
   apt-get -y dist-upgrade; \
-  apt-get -y install locales less dialog apt-utils man-db bc guile-3.0 zsh fish zip unzip sudo tmux bat vim emacs-nox git iproute2 procps lsb-release libnss3-tools curl httpie jq sqlite3 docker.io docker-compose; \
+  apt-get -y install locales less dialog apt-utils man-db bc guile-3.0 zsh fish zip unzip sudo tmux bat parallel vim emacs-nox git iproute2 procps lsb-release libnss3-tools curl httpie jq sqlite3 docker.io docker-compose; \
   apt-get -y install build-essential autoconf automake cmake m4 bison flex gettext; \
   apt-get -y install libssl-dev libcurl4-openssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev libelf-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl; \
   apt-get -y install libgmp-dev; \
