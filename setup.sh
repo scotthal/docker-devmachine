e858c73bea5a8aeda2b6e9113c9c96ecd775be24
Scott Hallock <scott@hallocks.net>
2024-11-17 13:43:36 -0700

    Remove Chrome and IntelliJ from default install
    They are now in homedir setup- scripts.

--- setup.sh
+++ setup.sh
@@ -61,19 +61,6 @@ then
     systemctl stop gdm.service; \
     systemctl disable gdm.service; \
     update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper
-
-  curl -L https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /tmp/crd.deb; \
-    dpkg -i /tmp/crd.deb; \
-    apt-get -y install -f; \
-    rm -f /tmp/crd.deb; \
-    echo 'dbus-launch startxfce4' > /etc/chrome-remote-desktop-session; \
-    curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb; \
-    dpkg -i /tmp/chrome.deb; \
-    apt-get -y install -f; \
-    rm -f /tmp/chrome.deb; \
-    update-alternatives --set x-www-browser /usr/bin/google-chrome-stable; \
-
-    snap install intellij-idea-community --classic
 fi
 
 apt-get -y remove exim4-base postfix; \
